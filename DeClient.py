838927a1c56e032e852233cbf3cd508656d82802
VerTeX <62620697+pashokkok@users.noreply.github.com>
2022-09-03 17:56:15 +0300

    Add files via upload

--- DeClient.py
+++ DeClient.py
@@ -4,6 +4,8 @@ G = '\033[32m' # green
 C = '\033[36m' # cyan
 W = '\033[0m'  # white
 
+import urllib.request
+import webbrowser
 import random
 from datetime import datetime
 import getpass
@@ -22,8 +24,9 @@ import smtplib
 import multiprocessing
 from os import system
 import re
-version = '1.3'
+version = '1.4'
 time.sleep(3)
+os.system('termimage Decl.jpg')
 print(G + '[+]' + C + ' Проверка обновлений.....', end='')
 ver_url = 'https://raw.githubusercontent.com/pashokkok/DeClient/main/version.txt'
 try:
@@ -57,141 +60,120 @@ except Exception as e:
 	print('\n' + R + '[-]' + C + ' Исключение : ' + W + str(e))
 
 def choice():
-	os.system('termimage Decl.jpg')
 	print(C)
 	print('█▀▄ █▀▀ █▀▀ █░░ █ █▀▀ █▄░█ ▀█▀')
 	print('█▄▀ ██▄ █▄▄ █▄▄ █ ██▄ █░▀█ ░█░')
 	print("DeClient@" + getpass.getuser() + "          |  Support  |        ")
 	print("DeClient@" + getpass.getuser() + "   Termux |   Linux   | Windows")
-	print("DeClient@" + getpass.getuser() + "          |    1.3    |        ")
+	print("DeClient@" + getpass.getuser() + "          |    1.4    |        ")
 	print("DeClient@" + getpass.getuser() + "          |  DeClient |        ")
-	print(G + '[1]' + C + ' ActiveKey')
-	print(G + '[2]' + C + ' Cracker')
-	print(G + '[3]' + C + ' DeBomber')
-	print(G + '[4]' + C + ' Exit')
+	print(G + '[1]' + C + ' Tools')
+	print(G + '[2]' + C + ' DeBomber')
+	print(G + '[3]' + C + ' DeChec-[IP]')
+	print(G + '[4]' + C + ' DeChec-[NUM]')
+	print(G + '[5]' + C + ' Exit')
 	print('\n')
 	print(Fore.GREEN)
 	option = input('DeClient ==>  ')
 	if option == "1":
-		key = input ("Syst-Key: ")
-		
-		if key == "DeClientFree":
-			print( Fore.GREEN )
-			print('Active')
-			time.sleep(2)
-			lists = input ('[Exit] Exit \n[1] Gemail-hack \n[2] B0mb3r \n[3] Mail-Spammer \n[4] Brute_Force \n[5] Grab-Cam \n[6] Shark \n[7] CrossFire Ddos \n[8] FaceBook_Brute \n[9] AresBomb \n[10] Track-Em \n[11] Cardesc \n[12] Insanity-Framework \n[13] Entropy Toolkit \n[14] Airgeddon \n[15] ISniff-GPS \n[16] Trape \n[17] Kali Nethunter \n[18] XAttacker \n[19] WordSteal \n[20] SpiderFoot \n[21] Instashell \n[22]VkFisher \n[23]Entropy \nDeClient ==>  ')
-			
-			if lists == "Exit":
-				exit()
-				
-			if lists == "exit":
-				exit()
-				
-			if lists == "1":
-				os.system('git clone https://github.com/Ha3MrX/Gemail-Hack')
-				
-			if lists == "2":
-				os.system('git clone https://github.com/Denishnc/b0mb3r')
-				
-			if lists == "3":
-				os.system('git clone https://github.com/pashokkok/Mail_Smmp')
-			
-			if lists == "4":
-				os.system('git clone https://github.com/Tim55667757/pwd_brut')
-				
-			if lists == "5":
-				os.system('git clone https://github.com/noob-hackers/grabcam')
-			
-			if lists == "6":
-				os.system('git clone https://github.com/Bhaviktutorials/shark')
-				
-			if lists == "7":
-				os.system('git clone https://github.com/crossfireTeam/crossfireMSDOS')
-				os.system('cd crossfireMSDOS')
-				os.system('cmake CMakeLists.txt')
-				os.system('chmod +x Makefile')
-				
-			if lists == "8":
-				os.system('git clone https://github.com/pashokkok/FB')
-				
-			if lists == "9":
-				os.system("pkg install git")
-				os.system("pkg install python")
-				os.system("pip install requests")
-				os.system("git clone https://github.com/MaksPV/AresBomb")
-				
-			if lists == "10":
-				os.system("git clone https://github.com/LiNuX-Mallu/Track-Em.git")
-				
-			if lists == "11":
-				os.system("git clone https://github.com/escdroid/cardesc.git")
-				
-			if lists == "12":
-				os.system("git clone https://github.com/4w4k3/Insanity-Framework")
-			
-			if lists == "13":
-				os.system("pip install tailer")
-				os.system("git clone https://github.com/evildevill/entropy")
-				
-			if lists == "14":
-				os.system("git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git")
-				
-			if lists == "15":
-				os.system("git clone https://github.com/hubert3/iSniff-GPS.git")
-			
-			if lists == "16":
-				os.system("git clone https://github.com/jofpin/trape.git")
-				
-			if lists == "17":
-				os.system("pkg install wget")
-				os.system("termux-setup-storage")
-				os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
-				os.system("chmod 777 install-nethunter-termux")
-				os.system("./install-nethunter-termux")
-				
-			if lists == "18":
-				os.system("git clone https://github.com/Moham3dRiahi/XAttacker.git")
-				
-			if lists == "19":
-				os.system("git clone https://github.com/0x09AL/WordSteal.git")
-				
-			if lists == "20":
-				os.system("git clone https://github.com/smicallef/spiderfoot")
-				
-			if lists == "21":
-				os.system("git clone https://github.com/tiptoettt/instashell")
-				
-			if lists == "22":
-				os.system("git clone https://github.com/foxlitegor/fisher")
-				
-			if lists == "23":
-				os.system("git clone https://github.com/evildevill/entropy")
-				
-			if lists == "24":
-				os.system("git clone https://github.com/sc1341/TikTok-OSINT")
-
-			else:
-				print('ERROR: 102' + Fore.YELLOW + '$' + Fore.RED + 'Invalid Tool')
-		
-		else:
-			print( Fore.RED )
-			print('ERROR: 101' + Fore.YELLOW + '$' + Fore.RED + 'Invalid Syst-Key')
-
-	if option == "2" :
-		os.system('clear')
-		print('Tipo Xaker :3')
+		print( Fore.GREEN )
+		print('Tools')
 		time.sleep(2)
-		print(Fore.GREEN)
-		print('Openning Matrix')
-		time.sleep(1)
-		print('Starting Tools')
-		time.sleep,(3)
-		print('Starting Cracker.py')
-		time.sleep(3)
-		os.system('pkg install cmatrix')
-		os.system('cmatrix')
+		lists = input ('[Exit] Exit \n[1] Gemail-hack \n[2] B0mb3r \n[3] Mail-Spammer \n[4] Brute_Force \n[5] Grab-Cam \n[6] Shark \n[7] CrossFire Ddos \n[8] FaceBook_Brute \n[9] AresBomb \n[10] Track-Em \n[11] Cardesc \n[12] Insanity-Framework \n[13] Entropy Toolkit \n[14] Airgeddon \n[15] ISniff-GPS \n[16] Trape \n[17] Kali Nethunter \n[18] XAttacker \n[19] WordSteal \n[20] SpiderFoot \n[21] Instashell \n[22] VkFisher \n[23] Entropy \nDeClient ==>  ')
+		
+		if lists == "Exit":
+			exit()
+			
+		if lists == "exit":
+			exit()
+			
+		if lists == "1":
+			os.system('git clone https://github.com/Ha3MrX/Gemail-Hack')
+			
+		if lists == "2":
+			os.system('git clone https://github.com/Denishnc/b0mb3r')
+			
+		if lists == "3":
+			os.system('git clone https://github.com/pashokkok/Mail_Smmp')
+		
+		if lists == "4":
+			os.system('git clone https://github.com/Tim55667757/pwd_brut')
+			
+		if lists == "5":
+			os.system('git clone https://github.com/noob-hackers/grabcam')
+		
+		if lists == "6":
+			os.system('git clone https://github.com/Bhaviktutorials/shark')
+			
+		if lists == "7":
+			os.system('git clone https://github.com/crossfireTeam/crossfireMSDOS')
+			os.system('cd crossfireMSDOS')
+			os.system('cmake CMakeLists.txt')
+			os.system('chmod +x Makefile')
+			
+		if lists == "8":
+			os.system('git clone https://github.com/pashokkok/FB')
+			
+		if lists == "9":
+			os.system("pkg install git")
+			os.system("pkg install python")
+			os.system("pip install requests")
+			os.system("git clone https://github.com/MaksPV/AresBomb")
+			
+		if lists == "10":
+			os.system("git clone https://github.com/LiNuX-Mallu/Track-Em.git")
+			
+		if lists == "11":
+			os.system("git clone https://github.com/escdroid/cardesc.git")
+			
+		if lists == "12":
+			os.system("git clone https://github.com/4w4k3/Insanity-Framework")
+		
+		if lists == "13":
+			os.system("pip install tailer")
+			os.system("git clone https://github.com/evildevill/entropy")
+			
+		if lists == "14":
+			os.system("git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git")
+			
+		if lists == "15":
+			os.system("git clone https://github.com/hubert3/iSniff-GPS.git")
+		
+		if lists == "16":
+			os.system("git clone https://github.com/jofpin/trape.git")
+			
+		if lists == "17":
+			os.system("pkg install wget")
+			os.system("termux-setup-storage")
+			os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+			os.system("chmod 777 install-nethunter-termux")
+			os.system("./install-nethunter-termux")
+			
+		if lists == "18":
+			os.system("git clone https://github.com/Moham3dRiahi/XAttacker.git")
+			
+		if lists == "19":
+			os.system("git clone https://github.com/0x09AL/WordSteal.git")
+			
+		if lists == "20":
+			os.system("git clone https://github.com/smicallef/spiderfoot")
+			
+		if lists == "21":
+			os.system("git clone https://github.com/tiptoettt/instashell")
+			
+		if lists == "22":
+			os.system("git clone https://github.com/foxlitegor/fisher")
+			
+		if lists == "23":
+			os.system("git clone https://github.com/evildevill/entropy")
+			
+		if lists == "24":
+			os.system("git clone https://github.com/sc1341/TikTok-OSINT")
 
-	if option == "3":
+		else:
+			print('ERROR: 102' + Fore.YELLOW + '$' + Fore.RED + 'Invalid Tool')
+		
+	if option == "2":
 		_name = ""
 		_phone9 = ""
 		newk = 0
@@ -856,7 +838,16 @@ def choice():
 		    except:
 		        pass
 
-	if option == "4" :
+	if option == "3" :
+		print("Started")
+		os.system("python ipdll.py")
+		
+	if option == "4":
+		print("Started")
+		numph = input("Номер телефона: +")
+		os.system("python numdll.py -n" + numph)
+
+	if option == "5" :
 		exit()
 		
 try:
